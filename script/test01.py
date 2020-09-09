592db3bbddf5ee5613cd238102a856eab7782e1d
Brucejc20 <brucelijc20@gmail.com>
2020-09-09 23:50:32 +0800

    first commit

---
+++ script/test01.py
@@ -0,0 +1,16 @@
+import allure
+import pytest
+
+class test_allure:
+    def test_01(self):
+        print("this is test case 01")
+        assert 1
+
+    def test_02(self):
+        print("this is test case 02")
+        assert 1
+
+    def test_03(self):
+        print("this is test case 03")
+        assert 0
+
